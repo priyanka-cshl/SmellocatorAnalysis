8dbeb488bf387b006458d0a1b4f92be4e352e5c2
priyanka-cshl <speak2priya@gmail.com>
2021-12-02 17:49:07 -0500

    adding PSTH view

--- LeverTaskUtils/RecordingSessionOverview.m
+++ LeverTaskUtils/RecordingSessionOverview.m
@@ -1,8 +1,20 @@
-function [] = RecordingSessionOverview(SingleUnits,SpikeHeight)
-    
-    if nargin<2
-        SpikeHeight = 0.8;
-    end
+function [] = RecordingSessionOverview(SingleUnits,varargin)
+
+%% parse input arguments
+narginchk(1,inf)
+params = inputParser;
+params.CaseSensitive = false;
+params.addParameter('rastermode', true, @(x) islogical(x) || x==0 || x==1);
+params.addParameter('spikeheight', 0.8, @(x) isnumeric(x) && x>0 && x<=1);
+params.addParameter('sessionlength', 100, @(x) isnumeric(x));
+params.addParameter('FRnormalizer', 20, @(x) isnumeric(x));
+
+% extract values from the inputParser
+params.parse(varargin{:});
+raster = params.Results.rastermode;
+SpikeHeight = params.Results.spikeheight;
+PSTHWindow = [0 1000*params.Results.sessionlength];
+divideFRby = params.Results.FRnormalizer;
     
     for i = 1:size(SingleUnits,2)
         foo(i,:) = [SingleUnits(i).tetrode SingleUnits(i).id];
@@ -19,6 +31,12 @@ function [] = RecordingSessionOverview(SingleUnits,SpikeHeight)
             MyColors = circshift(MyColors,1);
             whichtetrode = thistetrode;
         end
-        PlotRaster(SingleUnits(whichunit).spikes,i,MyColors(1,:),SpikeHeight);
+        if raster
+            PlotRaster(SingleUnits(whichunit).spikes,i,MyColors(1,:),SpikeHeight);
+        else
+            FR = MakePSTH(SingleUnits(whichunit).spikes', 0, PSTHWindow);
+            FR = i + FR/divideFRby;
+            plot((1:numel(FR))/1000,FR,'color',MyColors(1,:));
+        end
     end
 end
--- ViewerGUI/SessionViewer.m
+++ ViewerGUI/SessionViewer.m
@@ -22,7 +22,7 @@ function varargout = SessionViewer(varargin)
 
 % Edit the above text to modify the response to help SessionViewer
 
-% Last Modified by GUIDE v2.5 01-Dec-2021 16:55:38
+% Last Modified by GUIDE v2.5 02-Dec-2021 16:31:43
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -56,8 +56,9 @@ function SessionViewer_OpeningFcn(hObject, eventdata, handles, varargin)
 handles.output = hObject;
 
 % defaults
-handles.SessionLength = 100;
-
+handles.SampleRate = 500;
+handles.SessionLength.String = '100';
+handles.WhereSession.String = '/mnt/grid-hs/mdussauz/Smellocator/Processed/Behavior/O3/O3_20210922_r0_processed.mat';
 % Update handles structure
 guidata(hObject, handles);
 
@@ -84,9 +85,15 @@ function Scroller_Callback(hObject, eventdata, handles)
 % Hints: get(hObject,'Value') returns position of slider
 %        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
 
-newLims = get(hObject,'Value')*handles.SessionLength + [0 str2double(handles.TimeWindow.String)];
+newLims = handles.Scroller.Value * str2double(handles.SessionLength.String) + ...
+    [0 str2double(handles.TimeWindow.String)];
 set(handles.SpikesPlot,'XLim',newLims);
+set(handles.PSTHPlot,'XLim',newLims);
 set(handles.BehaviorPlot,'XLim',newLims); 
+set(handles.MotorPlot,'XLim',handles.SampleRate*newLims); 
+
+% Update handles structure
+guidata(hObject, handles);
 
 % --- Executes during object creation, after setting all properties.
 function Scroller_CreateFcn(hObject, eventdata, handles)
@@ -127,39 +134,64 @@ function LoadSession_Callback(hObject, eventdata, handles)
 % hObject    handle to LoadSession (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-[WhichSession, SessionPath] = uigetfile(...
-                                '/mnt/grid-hs/mdussauz/Smellocator/Processed/Behavior/O5/O5_20210923_r0_processed.mat',...
+
+if isempty(handles.WhereSession.String)
+        [WhichSession, SessionPath] = uigetfile(...
+                                '/mnt/grid-hs/mdussauz/Smellocator/Processed/Behavior/O3/O3_20210922_r0_processed.mat',...
                                 '*.mat', 'Select Behavior/Recording Session');
-                            
+handles.WhereSession.String = fullfile(SessionPath,WhichSession);
+end
+
 % Load the relevant variables
-load(fullfile(SessionPath,WhichSession), 'Traces', 'PassiveReplayTraces', 'TrialInfo', 'TargetZones', ...
+load(handles.WhereSession.String, 'Traces', 'PassiveReplayTraces', 'TrialInfo', 'TargetZones', ...
                'startoffset', 'errorflags', 'SampleRate', ...
                'TTLs', 'ReplayTTLs', 'TuningTTLs', 'SingleUnits');
 
-handles.SessionLength = 10*ceil(TTLs.Trial(end,2)/10);
-handles.TotalUnits = size(SingleUnits,2);
+handles.SessionLength.String = num2str(10*ceil(TTLs.Trial(end,2)/10));
+handles.NumUnits.String = num2str(size(SingleUnits,2));
+handles.SampleRate = SampleRate;
 [TracesOut] = ConcatenateTraces(Traces, 1:length(TrialInfo.TrialID), SampleRate*startoffset);
+
+% create a corresponding timestamp vector
+Timestamps = (1:numel(TracesOut.Lever{1}))/SampleRate;
+% make the first timestamp equal to first trial minus startoffset - such
+% that they match raw session timestamps
+Timestamps = Timestamps + TrialInfo.SessionTimestamps(1,1) - startoffset;
+
+% calculate the timestamp difference between Ephys and Behavior
 TrialStart_behavior = TrialInfo.SessionTimestamps(1,1);
 TrialStart_Ephys = TTLs.Trial(1,1);
-% convert all behavior timestamps to match Ephys
+% factor to convert all behavior timestamps to match Ephys
 TimestampAdjuster = TrialStart_Ephys - TrialStart_behavior;
 
-% plot odor boxes on the behavior plot
+%% plot odor boxes on the behavior plot
 axes(handles.BehaviorPlot);
-for i = 1:3
+hold off
+for i = 1:4
     handles.(['Trial',num2str(i),'Plot']) = fill(NaN,NaN,Plot_Colors(['Odor',num2str(i)]));
     hold on;
     handles.(['Trial',num2str(i),'Plot']).EdgeColor = 'none';
     ValveTS = TrialInfo.SessionTimestamps((TrialInfo.Odor==i),1:2)' + TimestampAdjuster;
-    handles.(['Trial',num2str(i),'Plot']).Vertices = [ ...
-        reshape([ValveTS(:) ValveTS(:)]', 2*numel(ValveTS), []) , ...
-        repmat([0 5 5 0]',size(ValveTS,2),1)];
-    handles.(['Trial',num2str(i),'Plot']).Faces = reshape(1:2*numel(ValveTS),4,size(ValveTS,2))';
+    if ~isempty(ValveTS)
+        handles.(['Trial',num2str(i),'Plot']).Vertices = [ ...
+            reshape([ValveTS(:) ValveTS(:)]', 2*numel(ValveTS), []) , ...
+            repmat([0 5 5 0]',size(ValveTS,2),1)];
+        handles.(['Trial',num2str(i),'Plot']).Faces = reshape(1:2*numel(ValveTS),4,size(ValveTS,2))';
+    end
 end
+
+% plot the target zone
+handles.TargetZonePlot = fill(NaN,NaN,Plot_Colors('TZ'),'FaceAlpha',0.2);
+hold on;
+handles.TargetZonePlot.EdgeColor = 'none';
+TrialTS = TrialInfo.SessionTimestamps(:,1:2)' + TimestampAdjuster;
+TZList =  TargetZones(TrialInfo.TargetZoneType,[3 1 1 3])';
+handles.TargetZonePlot.Vertices = [ ...
+        reshape([TrialTS(:) TrialTS(:)]', 2*numel(TrialTS), []) , ...
+        TZList(:)];
+handles.TargetZonePlot.Faces = reshape(1:2*numel(TrialTS),4,size(TrialTS,2))';
+    
 % plot the lever trace on top
-Timestamps = (1:numel(TracesOut.Lever{1}))/SampleRate;
-% make the first timestamp equal to first trial minus startoffset
-Timestamps = Timestamps - Timestamps(1) + TrialInfo.SessionTimestamps(1,1) - startoffset;
 plot(Timestamps + TimestampAdjuster, TracesOut.Lever{1},'k');
 
 % plot Rewards
@@ -175,8 +207,9 @@ set(handles.reward_plot,'XData',tick_x,'YData',tick_y);
 set(gca,'YLim', [0 8], 'YTick', [],...
     'XTick', [], 'XLim', [0 str2double(handles.TimeWindow.String)]);
 
-% plot odor boxes on the spikes plot
+%% plot odor boxes on the spikes plot
 axes(handles.SpikesPlot);
+hold off
 for i = 1:3
     handles.(['Odor',num2str(i),'Plot']) = fill(NaN,NaN,Plot_Colors(['Odor',num2str(i)]));
     hold on;
@@ -184,9 +217,20 @@ for i = 1:3
     ValveTS = TTLs.(['Odor',num2str(i)])(:,1:2)';
     handles.(['Odor',num2str(i),'Plot']).Vertices = [ ...
         reshape([ValveTS(:) ValveTS(:)]', 2*numel(ValveTS), []) , ...
-        repmat(handles.TotalUnits*[0 1 1 0]',size(ValveTS,2),1)];
+        repmat((10+str2double(handles.NumUnits.String))*[0 1 1 0]',size(ValveTS,2),1)];
     handles.(['Odor',num2str(i),'Plot']).Faces = reshape(1:2*numel(ValveTS),4,size(ValveTS,2))';
 end
+
+% plot air periods for the tuning period
+i = 4;
+handles.(['Odor',num2str(i),'Plot']) = fill(NaN,NaN,Plot_Colors(['Odor',num2str(i)]));
+hold on;
+handles.(['Odor',num2str(i),'Plot']).EdgeColor = 'none';
+ValveTS = TuningTTLs(TuningTTLs(:,5)==1,[4 6])';
+handles.(['Odor',num2str(i),'Plot']).Vertices = [ ...
+    reshape([ValveTS(:) ValveTS(:)]', 2*numel(ValveTS), []) , ...
+    repmat((10+str2double(handles.NumUnits.String))*[0 1 1 0]',size(ValveTS,2),1)];
+handles.(['Odor',num2str(i),'Plot']).Faces = reshape(1:2*numel(ValveTS),4,size(ValveTS,2))';
 
 % plot Rewards
 handles.water_plot = plot(NaN, NaN, 'color',Plot_Colors('t'),'Linewidth',1.25);
@@ -194,13 +238,175 @@ tick_timestamps =  TTLs.Reward(:,1);
 tick_x = [tick_timestamps'; tick_timestamps'; ...
     NaN(1,numel(tick_timestamps))]; % creates timestamp1 timestamp1 NaN timestamp2 timestamp2..
 tick_x = tick_x(:);
-tick_y = repmat( [0; handles.TotalUnits; NaN],...
+tick_y = repmat( [0; 10+str2double(handles.NumUnits.String); NaN],...
     numel(tick_timestamps),1); % creates y1 y2 NaN y1 timestamp2..
 set(handles.water_plot,'XData',tick_x,'YData',tick_y);
 
 % plot all spikes
 RecordingSessionOverview(SingleUnits);
-set(gca,'YLim', [0 handles.TotalUnits], 'YTick', [],...
+set(gca,'YLim', [0 10+str2double(handles.NumUnits.String)+1], 'YTick', [],...
     'TickDir','out','XLim', [0 str2double(handles.TimeWindow.String)]);
+
+
+%% plot odor boxes on the PSTH plot
+axes(handles.PSTHPlot);
+hold off
+for i = 1:3
+    handles.(['Smell',num2str(i),'Plot']) = fill(NaN,NaN,Plot_Colors(['Odor',num2str(i)]));
+    hold on;
+    handles.(['Smell',num2str(i),'Plot']).EdgeColor = 'none';
+    ValveTS = TTLs.(['Odor',num2str(i)])(:,1:2)';
+    handles.(['Smell',num2str(i),'Plot']).Vertices = [ ...
+        reshape([ValveTS(:) ValveTS(:)]', 2*numel(ValveTS), []) , ...
+        repmat((10+str2double(handles.NumUnits.String))*[0 1 1 0]',size(ValveTS,2),1)];
+    handles.(['Smell',num2str(i),'Plot']).Faces = reshape(1:2*numel(ValveTS),4,size(ValveTS,2))';
+end
+
+% plot air periods for the tuning period
+i = 4;
+handles.(['Smell',num2str(i),'Plot']) = fill(NaN,NaN,Plot_Colors(['Odor',num2str(i)]));
+hold on;
+handles.(['Smell',num2str(i),'Plot']).EdgeColor = 'none';
+ValveTS = TuningTTLs(TuningTTLs(:,5)==1,[4 6])';
+handles.(['Smell',num2str(i),'Plot']).Vertices = [ ...
+    reshape([ValveTS(:) ValveTS(:)]', 2*numel(ValveTS), []) , ...
+    repmat((10+str2double(handles.NumUnits.String))*[0 1 1 0]',size(ValveTS,2),1)];
+handles.(['Smell',num2str(i),'Plot']).Faces = reshape(1:2*numel(ValveTS),4,size(ValveTS,2))';
+
+% plot Rewards
+handles.h2o_plot = plot(NaN, NaN, 'color',Plot_Colors('t'),'Linewidth',1.25);
+tick_timestamps =  TTLs.Reward(:,1);
+tick_x = [tick_timestamps'; tick_timestamps'; ...
+    NaN(1,numel(tick_timestamps))]; % creates timestamp1 timestamp1 NaN timestamp2 timestamp2..
+tick_x = tick_x(:);
+tick_y = repmat( [0; 10+str2double(handles.NumUnits.String); NaN],...
+    numel(tick_timestamps),1); % creates y1 y2 NaN y1 timestamp2..
+set(handles.h2o_plot,'XData',tick_x,'YData',tick_y);
+
+% plot all PSTHs
+RecordingSessionOverview(SingleUnits,'rastermode',0,'sessionlength',str2double(handles.SessionLength.String));
+
+set(gca,'YLim', [0 10+str2double(handles.NumUnits.String)+1], 'YTick', [],...
+    'TickDir','out','XLim', [0 str2double(handles.TimeWindow.String)]);
+
+
+%% update the motor plot
+axes(handles.MotorPlot);
+hold off
+MotorTrajectory = zeros(str2double(handles.SessionLength.String)*SampleRate,1);
+% Fill in the Motor Data from the behavior session period
+Indices = round((Timestamps + TimestampAdjuster)*SampleRate);
+MotorTrajectory(Indices,1) = TracesOut.Motor{1}/100;
+
+% add the tuning period motor locations as well
+for x = 1:size(TuningTTLs,1)
+    if TuningTTLs(x,4) && ~isnan(TuningTTLs(x,5)) && ~isnan(TuningTTLs(x,7))
+        Indices = round(TuningTTLs(x,4)*SampleRate):round(TuningTTLs(x,6)*SampleRate);
+        MotorTrajectory(Indices,1) = TuningTTLs(x,7)/100;
+    end
+end
+
+handles.MotorTrajectoryPlot = imagesc(MotorTrajectory',[-1 1]);
+colormap(brewermap(100,'rdbu'));
+
+set(gca, 'YTick', [], 'XTick', [], ...
+    'TickDir','out','XLim', [0 SampleRate*str2double(handles.TimeWindow.String)]);
+
+% display PSTH or Rasters as needed
+PSTHView_Callback(hObject, eventdata, handles);
+
 % Update handles structure
 guidata(hObject, handles);
+
+
+
+function WhereSession_Callback(hObject, eventdata, handles)
+% hObject    handle to WhereSession (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of WhereSession as text
+%        str2double(get(hObject,'String')) returns contents of WhereSession as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function WhereSession_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to WhereSession (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function NumUnits_Callback(hObject, eventdata, handles)
+% hObject    handle to NumUnits (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of NumUnits as text
+%        str2double(get(hObject,'String')) returns contents of NumUnits as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function NumUnits_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to NumUnits (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function SessionLength_Callback(hObject, eventdata, handles)
+% hObject    handle to SessionLength (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of SessionLength as text
+%        str2double(get(hObject,'String')) returns contents of SessionLength as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function SessionLength_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to SessionLength (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in NewSession.
+function NewSession_Callback(hObject, eventdata, handles)
+% hObject    handle to NewSession (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+handles.WhereSession.String = '';
+
+
+% --- Executes on button press in PSTHView.
+function PSTHView_Callback(hObject, eventdata, handles)
+% hObject    handle to PSTHView (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of PSTHView
+if handles.PSTHView.Value
+    set([handles.SpikesPlot; handles.SpikesPlot.Children], 'Visible','off');
+    set([handles.PSTHPlot; handles.PSTHPlot.Children], 'Visible','on');
+else
+    set([handles.SpikesPlot; handles.SpikesPlot.Children], 'Visible','on');
+    set([handles.PSTHPlot; handles.PSTHPlot.Children], 'Visible','off');
+end
