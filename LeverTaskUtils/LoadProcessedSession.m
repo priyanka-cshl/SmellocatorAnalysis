ed85af8b79c46779e16cd8b9de861130b234a167
priyanka-cshl <speak2priya@gmail.com>
2022-02-01 17:38:13 -0500

    extracting session traces in a more intelligent way

--- LeverTaskUtils/LoadProcessedSession.m
+++ LeverTaskUtils/LoadProcessedSession.m
@@ -18,3 +18,57 @@ load(MySession, 'Traces', 'PassiveReplayTraces', 'TrialInfo', 'TargetZones', ...
 
 %SessionLength = 10*ceil(TTLs.Trial(end,2)/10);
 %NumUnits = size(SingleUnits,2);
+
+%% Concatenate traces and get one matrix with all behavior variables
+% SampleRate = behavior sample rate;
+[TracesOut, ColNames] = ConcatenateTraces2Mat(Traces, 1:length(TrialInfo.TrialID), SampleRate*startoffset);
+clear Traces
+% create a corresponding timestamp vector
+Timestamps = TracesOut(:,find(strcmp(ColNames,'Timestamps')))'; % in behavior timebase
+
+% calculate the timestamp difference between Ephys and Behavior
+TrialStart_behavior = TrialInfo.SessionTimestamps(1,2);
+TrialStart_Ephys = TTLs.Trial(1,2);
+% factor to convert all behavior timestamps to match Ephys
+TimestampAdjuster = TrialStart_Ephys - TrialStart_behavior; % add to behavior TS to convert to OEPS
+
+%% Process the TrialOn vector to add to it OdorOn periods in -ve
+TrialTrace = TracesOut(:,find(strcmp(ColNames,'Trial')))';
+% get trial ON-OFF indices
+Idx = [find(diff(TrialTrace>0)==1)'+1 find(diff(TrialTrace>0)==-1)'];
+% get OdorStart Times w.r.t. Trial start (from the behavior file)
+Idx(:,3) = Idx(:,1) + ceil(SampleRate*TrialInfo.OdorStart(whichTrials,2));
+if ~isempty(TTLs)
+    % OdorStart Times can also be extracted from OEPS TTLs
+    Idx(:,4) = Idx(:,1) + ceil(SampleRate*TTLs.Trial(whichTrials,4));
+    if any(abs(Idx(:,3)-Idx(:,4))>5)
+        disp('mismatch in OdorOn Timestamps between behavior and OEPS files');
+        keyboard;
+    end
+end
+    temp = 0*TrialTrace;
+    x1 = 1;
+    for k = 1:size(Idx,1)
+        % TargetZone vector
+        x2 = Idx(k,2);
+        temp(x1:x2,1) = AllTargets(TrialInfo.TargetZoneType(whichTrials(k)));
+        x1 = x2 + 1;
+        
+        % OdorON + TrialON vector
+        TrialTrace(Idx(k,3):Idx(k,1)-1,1) = -TrialTrace(Idx(k,1));
+    end
+
+
+
+%% Split the trial vector into the three odors
+for i = 1:3
+    Trial = TracesOut(:,find(strcmp(ColNames,'Trial')))';
+    Trial(Trial~=i) = 0;
+    Trial(Trial>0) = 1;
+    Motor = TracesOut(:,find(strcmp(ColNames,'Motor')))';
+    Motor(Trial==0) = NaN;
+    Motor = 125 - Motor;
+    Motor(Trial==0) = 0;
+    TracesOut(:,end+1) = Motor;
+    ColNames{end+1} = ['Odor',num2str(i)];
+end
