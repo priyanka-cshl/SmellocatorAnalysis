5ab9b1631a845d3526444cfc0380aea9c5de0a84
priyanka gupta <speak2priya@gmail.com>
2022-02-06 14:41:48 -0500

    debugging passive replay

--- LeverTaskUtils/TrialAlignedSpikeTimes.m
+++ LeverTaskUtils/TrialAlignedSpikeTimes.m
@@ -10,13 +10,21 @@ for whichunit = 1:N % every unit
         if whichtrial>1
             previousTrialspikes = thisUnitspikes(trialtags==whichtrial-1) - ...
                 (TTLs.Trial(whichtrial,1) - TTLs.Trial(whichtrial-1,1));
+            
+            % ignore spikes that happened before previous trial off
+            prevToff = TTLs.Trial(whichtrial-1,2) - TTLs.Trial(whichtrial,1);
+            if ~isempty(previousTrialspikes)
+                previousTrialspikes(previousTrialspikes<prevToff) = [];
+            end
         else
             previousTrialspikes = thisUnitspikes(trialtags==whichtrial-1) - ...
                 (TTLs.Trial(whichtrial,1) - 0);
         end
+        
 %         if ~isempty(previousTrialspikes)
 %             previousTrialspikes(previousTrialspikes<-1) = [];
 %         end
+
         AlignedSpikes{whichtrial,whichunit} = {horzcat(previousTrialspikes',thisTrialspikes')};
         if whichunit == 1
             x1 = TTLs.Trial(whichtrial,1);
