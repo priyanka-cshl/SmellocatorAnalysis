609be4876617fddec43f152a04ba2e9a774d2187
priyanka-cshl <speak2priya@gmail.com>
2023-03-17 17:39:03 -0400

    session list for easily rerunning preprocessing

---
+++ Scripts/sessionlist_OQ.m
@@ -0,0 +1,22 @@
+% open loop
+PreprocessSmellocatorData('Q4_20221112_r0.mat',1);
+PreprocessSmellocatorData('Q5_20221122_r0.mat',1);
+PreprocessSmellocatorData('Q8_20221209_r0.mat',1);
+PreprocessSmellocatorData('Q9_20221119_r0.mat',1);
+
+% halts
+PreprocessSmellocatorData('Q4_20221109_r0.mat',1);
+PreprocessSmellocatorData('Q8_20221207_r0.mat',1);
+
+% open loop
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O3/O3_20211005_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O7/O7_20220630_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O8/O8_20220702_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O9/O9_20220630_r0.mat',1);
+
+% halts
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O3/O3_20210927_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O3/O3_20210929_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O7/O7_20220702_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O8/O8_20220704_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O9/O9_20220702_r1.mat',1);
