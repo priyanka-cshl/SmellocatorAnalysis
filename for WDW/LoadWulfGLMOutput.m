cf196bf70fb9830466d13419ceab0018ce34bdbd
priyanka-cshl <21067042+priyanka-cshl@users.noreply.github.com>
2024-09-27 10:00:06 -0400

    getting predictions from kernels

--- for WDW/LoadWulfGLMOutput.m
+++ for WDW/LoadWulfGLMOutput.m
@@ -1,8 +1,28 @@
-function [PSTHOut] = LoadWulfGLMOutput(myPath,nUnits)
+function [PSTHOut,TimestampsOut] = LoadWulfGLMOutput(myPath,nUnits,TimestampsIn)
+
 load(myPath);
 
+interpolate = 0; 
+if nargin == 3
+    if ~isempty(TimestampsIn)
+        interpolate = 1;
+    end
+end
+
+Timestamps = full_active_y_pred_times;
+if ~interpolate
+    TimestampsOut = Timestamps;
+else
+    TimestampsOut = TimestampsIn;
+    scaling = mean(diff(TimestampsIn))/mean(diff(Timestamps));
+end
 for i = 1:nUnits
-    PSTHOut(i,:) = eval(['neuron_',num2str(i-1),'.full_test_y_pred']);
+    if ~interpolate
+        PSTHOut(i,:) = eval(['neuron_',num2str(i-1),'.full_active_y_pred']);
+    else
+        myPSTH = eval(['neuron_',num2str(i-1),'.full_active_y_pred']);
+        PSTHOut(i,:) = scaling*interp1(Timestamps,myPSTH,TimestampsIn);
+    end
 end
 
 end
