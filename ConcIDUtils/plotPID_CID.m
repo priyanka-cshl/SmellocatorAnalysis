70559f79dc9351a5713a7f2b0e6590ca9b868f46
priyanka-cshl <21067042+priyanka-cshl@users.noreply.github.com>
2026-07-29 15:31:07 -0400

    getting average and raw traces for both kinds of 16 odor expts - old and new

--- ConcIDUtils/plotPID_CID.m
+++ ConcIDUtils/plotPID_CID.m
@@ -1,33 +1,86 @@
 %% script to plot PID traces for CID sessions done on photoncerber 
 
-recDir = '/mnt/grid-hs/mdussauz/PID_test/2022-05-31_08-54-29_16Odor/Record Node 104';
-%recDir = '/mnt/grid-hs/mdussauz/PID_test/2022-05-30_18-19-56/Record Node 104'
+%recDir = '/mnt/grid-hs/mdussauz/CID/PID/2022-05-31_08-54-29_16Odor/Record Node 104';
+recDir = '/mnt/grid-hs/mdussauz/CID/PID/newCID/2025-05-14_12-10-02/Record Node 104';
+%recDir = '/mnt/grid-hs/mdussauz/CID/PID/newCID/2025-05-14_15-20-57/Record Node 104';
+
+% load the preprocessed data (or preprocess first)
 [PIDTrace, TTLs, StimSettings] = getPID_CID(recDir);
 
+%% organize by trial, stimulus etc
+
+% some settings
+SamplingRate = 500; % in Hz
+
+switch StimSettings.SessionType
+    case '16_Odors'
+        nStim = size(StimSettings.Odors,1); % no. of odors delivered
+        OdorList = StimSettings.Odors;
+        nTypes = numel(StimSettings.Dilutions); % concentrations used
+        ConcList = StimSettings.Dilutions;
+        window = [-StimSettings.timing(2) sum(StimSettings.timing(4:5))]/1000; % in seconds [-preOdor odor+post]
+    case 'newCID'
+        nStim = size(StimSettings.miniOdors,1) + size(StimSettings.megaOdors,1); % no. of odors delivered
+        OdorList = [StimSettings.miniOdors; StimSettings.megaOdors];
+        nTypes = numel(StimSettings.Dilutions); % concentrations used
+        ConcList = StimSettings.Dilutions;
+        window = [-StimSettings.timing(2) sum(StimSettings.timing(4:5))]/1000; % in seconds [-preOdor odor+post]
+    case 'ConcentrationSeries'
+        window = [-StimSettings.timing(2) sum(StimSettings.timing(4:5))]/1000; % in seconds [-preOdor odor+post]
+end
+
+window = window -1; % 1 sec buffer
+baselineWindow = 1*SamplingRate; % samples
+PIDOut = []; TimeTraceOut = []; SteadyState = []; MeanVals = []; TimeOut = []; prevOdor = [];
+
+for odor = 1:nStim % every odor
+    for conc = 1:nTypes % at every conc.
+        whichTrials = find( (TTLs.Trial(:,4) == OdorList(odor)) & (TTLs.Trial(:,5) == ConcList(conc)) );
+        for n = 1:numel(whichTrials)
+            t = TTLs.Trial(whichTrials(n),7:8); % odor start, odor stop
+            t = t + window;
+            [~,idx1] = min(abs(PIDTrace(:,1)-t(1)));
+            [~,idx2] = min(abs(PIDTrace(:,1)-t(2)));
+            thisTrialPID = PIDTrace(idx1:idx2,2);
+
+            PIDOut(n,1:numel(thisTrialPID),odor,conc) = thisTrialPID;
+            TimeTraceOut(n,1:numel(thisTrialPID),odor,conc) = PIDTrace(idx1:idx2,1);
+
+            if (whichTrials(n)-1)>0
+                prevOdor(n,1,odor,conc) = TTLs.Trial(whichTrials(n)-1,4);
+            else
+                prevOdor(n,1,odor,conc) = TTLs.Trial(whichTrials(n),4);
+            end
+
+            if odor == 1 && conc == 1 && n == 1
+                TimeOut = PIDTrace(idx1:idx2,1);
+                TimeOut = TimeOut - TTLs.Trial(whichTrials(n),7);
+            end
+
+            % preodor baseline
+            [~,idx3] = min(abs(PIDTrace(:,1)-TTLs.Trial(whichTrials(n),7)));
+            baselines(n,1,odor,conc) = median(PIDTrace(idx3+[-baselineWindow 0],2));
+        end
+    end
+end
+
+%% Plotting related settings
+
 if strcmp(StimSettings.SessionType,'ConcentrationSeries')
-stackConcs = 1;
-if stackConcs == 2
-    if strcmp(StimSettings.SessionType,'ConcentrationSeries')
+    stackConcs = 1;
+    if stackConcs == 2
         newStimCol = (TTLs.Trial(:,5)*10^4) + TTLs.Trial(:,4);
         newTypesCol = TTLs.Trial(:,5)*0;
         TTLs.Trial(:,4:5) = [newStimCol newTypesCol];
         StimSettings.SessionType = '16_Concs*';
     end
-end
-if stackConcs == 1
-    if strcmp(StimSettings.SessionType,'ConcentrationSeries')
+    if stackConcs == 1
         newStimCol = TTLs.Trial(:,5) + TTLs.Trial(:,4);
         newTypesCol = TTLs.Trial(:,5)*0;
         TTLs.Trial(:,4:5) = [newStimCol newTypesCol];
         StimSettings.SessionType = '16_Concs';
     end
 end
-end
-
-nStim = numel(unique(TTLs.Trial(:,4))); % no. of odors delivered
-OdorList = unique(TTLs.Trial(:,4));
-nTypes = numel(unique(TTLs.Trial(:,5))); % concentrations used
-ConcList = unique(TTLs.Trial(:,5));
 
 switch StimSettings.SessionType
     case {'newCID', '16_Odors'}
@@ -55,73 +108,62 @@ switch StimSettings.SessionType
         StimSettings.SessionType = '16_Odors';
 end
 
-%%
-window = [-StimSettings.timing(2) sum(StimSettings.timing(4:5))]/1000; % in seconds
-window = window -1;
-PIDOut = []; TimeTraceOut = []; SteadyState = []; MeanVals = []; TimeOut = []; prevOdor = [];
-for odor = 1:nStim
-    for conc = 1:nTypes
-        whichTrials    = find( (TTLs.Trial(:,4) == OdorList(odor)) & (TTLs.Trial(:,5) == ConcList(conc)) );
-        for n = 1:numel(whichTrials)
-            t = TTLs.Trial(whichTrials(n),7:8); % odor start, odor stop
-            t = t + window;
-            [~,idx1] = min(abs(PIDTrace(:,1)-t(1)));
-            [~,idx2] = min(abs(PIDTrace(:,1)-t(2)));
-            thisTrialPID = PIDTrace(idx1:idx2,2);
-            PIDOut(n,1:numel(thisTrialPID),odor,conc) = thisTrialPID;
-%             PIDvals{n,odor+1}  = PIDTrace(idx1:idx2,2);
-%             SteadyState(n,odor+1) = mean(PIDvals{n,odor+1}(end-99:end));
-            TimeTraceOut(n,1:numel(thisTrialPID),odor,conc) = PIDTrace(idx1:idx2,1);
-            if (whichTrials(n)-1)>0
-                prevOdor(n,1,odor,conc) = TTLs.Trial(whichTrials(n)-1,4);
-            else
-                prevOdor(n,1,odor,conc) = TTLs.Trial(whichTrials(n),4);
-            end
-            if odor == 1 && conc == 1 && n == 1
-                TimeOut = PIDTrace(idx1:idx2,1);
-                TimeOut = TimeOut - TTLs.Trial(whichTrials(n),7);
-            end
-        end
-    end
-end
-
 %% plotting
-%%
-% figure;
-% conc = 1;
-% for odor = 1:nStim
-%     subplot(4,4,odor);
-%     hold on
-%     for n = 1:size(PIDOut,1)
-%         thisTrialPID = [];
-%         thisTrialPID(:,2) = squeeze(PIDOut(n,:,odor,conc));
-%         thisTrialPID(1:numel(TimeOut),1) = TimeOut;
-%         thisTrialPID(numel(TimeOut)+1:end,:) = []; 
-%         plot(thisTrialPID(:,1),thisTrialPID(:,2),'Color',mycolors(odor,:));
-%     end
-% end
-
+trialgradient = 0;
+normalized = 0;
+baselinesubtraced = 1;
+if ~baselinesubtraced
+    normalized = 0;
+end
+AverageOut = [];
+traceLength = (diff(window)+(StimSettings.timing(3)/1000))*SamplingRate;
+% Plot the raw values, later trials are darker
 figure;
-conc = 1;
 for odor = 1:nStim
     subplot(4,4,odor);
     hold on
     YVals = [];
-    for n = 1:size(PIDOut,1)
-        thisTrialPID = [];
-        thisTrialPID(:,2) = squeeze(PIDOut(n,:,odor,conc));
-        thisTrialPID(1:numel(TimeOut),1) = TimeOut;
-        thisTrialPID(numel(TimeOut)+1:end,:) = []; 
-        thisTrialcolor = mycolors(odor,:) + (1 - mycolors(odor,:))*(1-n*0.2);
-        %plot(thisTrialPID(:,1),thisTrialPID(:,2),'Color',mycolors(odor,:));
-        plot(thisTrialPID(:,1),thisTrialPID(:,2),'Color',thisTrialcolor);
-        YVals(n,:) = [min(thisTrialPID(:,2)) max(thisTrialPID(:,2))];
+    for conc = 1:nTypes
+        AverageOut(odor,:,conc) = zeros(traceLength,1);
+        reps = 0;
+        for n = 1:size(PIDOut,1)
+            if baselines(n,1,odor,conc)
+                thisTrialPID = [];
+                thisTrialPID(:,2) = squeeze(PIDOut(n,:,odor,conc));
+                thisTrialPID(1:numel(TimeOut),1) = TimeOut;
+                untilIdx = find(squeeze(TimeTraceOut(n,:,odor,conc))==0,1,'first');
+                thisTrialPID(untilIdx:end,:) = [];
+                if baselinesubtraced
+                    thisTrialPID(:,2) = thisTrialPID(:,2) - baselines(n,1,odor,conc); % subtract baseline
+                end
+                if normalized
+                    thisTrialPID(:,2) = thisTrialPID(:,2)/max(thisTrialPID(:,2));
+                end
+                thisTrialPID(1:numel(TimeOut),1) = TimeOut;
+                thisTrialPID(numel(TimeOut)+1:end,:) = [];
+                if trialgradient
+                    thisTrialcolor = mycolors(odor,:) + (1 - mycolors(odor,:))*(1-n*0.2);
+                    plot(thisTrialPID(:,1),thisTrialPID(:,2),'Color',thisTrialcolor);
+                else
+                    plot(thisTrialPID(:,1),thisTrialPID(:,2),'Color',mycolors(odor,:));
+                end
+                YVals(n,:) = [min(thisTrialPID(:,2)) max(thisTrialPID(:,2))];
+                AverageOut(odor,:,conc) = AverageOut(odor,:,conc) + thisTrialPID(1:traceLength,2)';
+                reps = reps+1;
+            end
+        end
+        AverageOut(odor,:,conc) = AverageOut(odor,:,conc)/reps;
+        %plot(thisTrialPID(1:traceLength,1),squeeze(AverageOut(odor,:,conc)),'Color',mycolors(odor,:),'LineWidth',2);
+        plot(thisTrialPID(1:traceLength,1),squeeze(AverageOut(odor,:,conc)),'Color','k');
     end
     YVals(n+1,:) = [min(YVals(:,1)) max(YVals(:,2))];
     YVals(n+1,:) = YVals(n+1,:) + (abs(diff(YVals(n+1,:)))*0.1)*[-1 1];
     set(gca,'XLim',[-10 30],'YLim',YVals(n+1,:));
 end
 
+%%
+skip = 1
+if ~skip
 deltaT = TimeOut(end) - TimeOut(1) + 0.1;
 figure;
 conc = 1;
@@ -200,4 +242,4 @@ end
 %     MeanVals{odor+1}(count,1) = mean(SteadyState(f,odor+1));
 %     MeanVals{odor+1}(count,2) = std(SteadyState(f,odor+1));
 % end
-    
+end
